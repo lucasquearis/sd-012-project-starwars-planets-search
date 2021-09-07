d3d592e3c1d92da622dd95115bcaf720fb6f5604
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-09-07 18:52:45 -0300

    iniciando o deploy

---
+++ build/precache-manifest.f3f042fade7a7df9a625fcc29ac09efb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f556bbf8bddd8e08c6aa67f6499068aa",
+    "url": "/star-wars-project/index.html"
+  },
+  {
+    "revision": "e0aacfbcf718e81a4db6",
+    "url": "/star-wars-project/static/css/main.b4d7a9a4.chunk.css"
+  },
+  {
+    "revision": "54a158270d2bc4d591d9",
+    "url": "/star-wars-project/static/js/2.3147f4ab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-project/static/js/2.3147f4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0aacfbcf718e81a4db6",
+    "url": "/star-wars-project/static/js/main.dda1bebf.chunk.js"
+  },
+  {
+    "revision": "88082d819d89e019d559",
+    "url": "/star-wars-project/static/js/runtime-main.127fcb49.js"
+  },
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/star-wars-project/static/media/Starjhol.9c91297e.ttf"
+  },
+  {
+    "revision": "f5bdff6225b63923b571d039f42d4519",
+    "url": "/star-wars-project/static/media/star-wars-logo.f5bdff62.png"
+  }
+]);
